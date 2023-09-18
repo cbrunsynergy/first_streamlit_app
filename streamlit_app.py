b8c417f85d4cee1ba6811cd6366f7a9c16cbb4ba
cbrunsynergy <145336839+cbrunsynergy@users.noreply.github.com>
2023-09-18 11:51:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 
@@ -18,7 +17,13 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 # Définir 'Fruit' comme index
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Afficher le dataframe
+# Ajout d'une barre de recherche pour filtrer les fruits
+search_term = st.text_input("Search for fruits:")
+
+if search_term:
+    my_fruit_list = my_fruit_list[my_fruit_list.index.str.contains(search_term, case=False)]
+
+# Afficher le dataframe filtré
 st.dataframe(my_fruit_list)
 
 # Créer une liste déroulante pour sélectionner des fruits
