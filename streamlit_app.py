92d814b2111e55ef9a5be6d682f7b767a20e6deb
cbrunsynergy <145336839+cbrunsynergy@users.noreply.github.com>
2023-09-27 14:34:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,7 +119,7 @@ if streamlit.button('Add a Fruit to the List'):
 
  
 
-#import snowflake.connector
+import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 ## my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
